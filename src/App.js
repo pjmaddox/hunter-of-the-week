2a9dfd00bb4954331fde4c596d391184acdeb63b
pjmaddox24 <pjmaddox24@gmail.com>
2018-10-14 21:36:54 -0500

    more small placeholders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AddNewCharacterBarContainer from './components/containers/AddNewCharacterBarContainer';
+import AddNewCharacterBarContainer from './components/containers/AddNewCharacterBarContainer.js';
+import CharacterSelect from './components/containers/CharacterSelect.js';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CharacterSelect/>
         <AddNewCharacterBarContainer />
       </div>
     );
