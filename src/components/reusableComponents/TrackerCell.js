288a433641e1ece7eaab0305b7a6cbc78c8f32fa
pjmaddox24 <pjmaddox24@gmail.com>
2018-11-11 20:32:31 -0600

    -changed tracker cells around to be more visible but equally as ugly

--- src/components/reusableComponents/TrackerCell.js
+++ src/components/reusableComponents/TrackerCell.js
@@ -3,8 +3,9 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 const TrackerCell = (props) => {
     return (
-        <div className="trackerCell" style={{ backgroundColor: props.isFilled? props.fillColorText : "white", cursor: "pointer", border: "1px grey" }} onClick={props.onCellClick}>
+        <div className="trackerCell" style={{ backgroundColor: props.isFilled? props.fillColorText : "white", cursor: "pointer", border: "1px grey", height: "1rem", width: "1rem", display: "inline-block" }} onClick={props.onCellClick}>
             {/* TODO: how the hell to style this?!?!?!?!? */}
+            <input type="checkbox" checked={props.isFilled}/>
         </div>
     );
 };
