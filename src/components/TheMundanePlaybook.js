fe66938248620a3479701b456dc1450c46a7d0f0
pjmaddox24 <pjmaddox24@gmail.com>
2018-11-21 13:30:56 -0600

    -fixed mundane pb

--- src/components/TheMundanePlaybook.js
+++ src/components/TheMundanePlaybook.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TheMundanePlaybook = (props) => {
-    return {
-        (
+    return (
             <div className="theMundanePlaybookContainer">
 
             </div>
         );
-    };
 }
 
 export default TheMundanePlaybook;
