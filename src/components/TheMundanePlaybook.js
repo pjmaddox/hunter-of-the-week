22b0600b1a565871d2bc952141a3760768a6fba7
pjmaddox24 <pjmaddox24@gmail.com>
2018-11-21 13:29:39 -0600

    -added mundane plabook and PBContainer

---
+++ src/components/TheMundanePlaybook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TheMundanePlaybook = (props) => {
+    return {
+        (
+            <div className="theMundanePlaybookContainer">
+
+            </div>
+        );
+    };
+}
+
+export default TheMundanePlaybook;
---
+++ src/components/containers/TheMundanePlaybookContainer.js
@@ -0,0 +1,16 @@
+import connect from 'react-redux';
+import TheMundanePlaybook from "../TheMundanePlaybook.js";
+
+const mapStateToProps = state => {
+    return {
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    };
+};
+
+export default connect()();
