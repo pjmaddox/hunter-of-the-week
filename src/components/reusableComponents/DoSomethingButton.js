67baa12d07928940e30fb84941e31309359bf6d4
Patrick Maddox <patrick.maddox@centare.com>
2019-05-20 11:18:26 -0500

    removed component import

--- src/components/reusableComponents/DoSomethingButton.js
+++ src/components/reusableComponents/DoSomethingButton.js
@@ -1,5 +1,5 @@
 //DoSomethingButton - Reusable Component
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from 'prop-types';
 const DoSomethingButton = (props) => {
     return (
