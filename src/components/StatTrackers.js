85c0ab541c29a8ead64e41ef42ede796cf25254d
pjmaddox24 <pjmaddox24@gmail.com>
2019-05-27 20:01:31 -0500

    changed stat tracker to use Expansionpanel

--- src/components/StatTrackers.js
+++ src/components/StatTrackers.js
@@ -3,15 +3,24 @@ import HarmTrackerContainer from './containers/HarmTrackerContainer';
 import ExperienceTrackerContainer from './containers/ExperienceTrackerContainer';
 import LuckTrackerContainer from './containers/LuckTrackerContainer';
 import CollapsibleSection from "./reusableComponents/CollapsibleSection";
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import Typography from '@material-ui/core/Typography';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const StatTracker = () => {
     return (
         <div className="statTracker">
-            <CollapsibleSection title={"Stat Trackers"}>
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel3bh-content">
+                    <Typography>Stat Trackers</Typography>
+                </ExpansionPanelSummary>
                 <HarmTrackerContainer />
                 <ExperienceTrackerContainer />
                 <LuckTrackerContainer />
-            </CollapsibleSection>
+            </ExpansionPanel>
         </div>
     );
 }
